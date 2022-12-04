c8b4a8af38cc2f72eea626fd5c009fb8aef884ac
Jamo <chillbillies@proton.me>
2022-12-04 16:33:17 -0700

    Cleans up solution to day 1

--- 2022/1/main.js
+++ 2022/1/main.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const util = require('util');
 
-const PUZZLE_TXT_INPUT = fs.readFile('puzzle-input', 'utf-8', function (err, data) {
+fs.readFile('puzzle-input', 'utf-8', function (err, data) {
 	let splitRations = data.split("\r\n");
 	let chunkIndex = 0;
 	let elfRations = splitRations.reduce((resultArray, item) => {
+		// Everytime we encounter an empty string, increase chunk index
 		if (item === "") {
 			chunkIndex += 1;
 		}
@@ -13,6 +14,7 @@ const PUZZLE_TXT_INPUT = fs.readFile('puzzle-input', 'utf-8', function (err, dat
 			resultArray[chunkIndex] = [];
 		}
 
+		// Add calorie count to the new array at chunkIndex (which corresponds to the "elf" index)
 		if (item !== "") {
 			resultArray[chunkIndex].push(parseInt(item))
 		}
@@ -20,28 +22,41 @@ const PUZZLE_TXT_INPUT = fs.readFile('puzzle-input', 'utf-8', function (err, dat
 		return resultArray
 	}, [])
 
-	//console.log(util.inspect(typeof test, { maxArrayLength: null }))
-	let elfWithMostRations = elfRations.reduce((elfWithMostRations, elfRations, currentIndex) => {
+	let topElf = getElfWithMostCalories(elfRations);
+	console.log("Elf " + topElf.index + " has the most calories: " + topElf.totalCalories);
+
+	elfRations.splice(topElf.index, 1);
+
+	let secondElf = getElfWithMostCalories(elfRations);
+
+	elfRations.splice(secondElf.index, 1);
+
+	let thirdElf = getElfWithMostCalories(elfRations);
+
+	console.log("Total calories from top 3 elves: " + (topElf.totalCalories + secondElf.totalCalories + thirdElf.totalCalories));
+});
+
+const getElfWithMostCalories = (allElfRations) => {
+	return allElfRations.reduce((elfWithMostCalories, elfRations, currentIndex) => {
 		let sumOfElfRations = elfRations.reduce((sum, ration) => {
 			return sum + ration;
 		}, 0);
 
-		let elf = {
-			id: currentIndex,
-			totalRations: sumOfElfRations
+		let currentElf = {
+			index: currentIndex,
+			totalCalories: sumOfElfRations
 		}
 
-		if (sumOfElfRations > elfWithMostRations.totalRations) {
-			elfWithMostRations = elf;
+		if (sumOfElfRations > elfWithMostCalories.totalCalories) {
+			elfWithMostCalories = currentElf;
 		}
 
-		return elfWithMostRations;
+		return elfWithMostCalories;
 
-	}, {id: 0, totalRations: 0});
+	}, {index: 0, totalCalories: 0});
+};
 
-	console.log(elfWithMostRations);
-});
-
+// Not used; but demonstrates the idea behind reduce()
 const sumRations = (rations) => {
 	return rations.reduce(reducer, 0);
 }

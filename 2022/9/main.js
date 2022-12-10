3cb31dfb394b7bb85f69ec644718ed6ea1230265
Jamison Griffith <jamison@zingstudios.com>
2022-12-09 17:00:14 -0700

    Adds more structure to day 9

--- 2022/9/main.js
+++ 2022/9/main.js
@@ -5,6 +5,24 @@ fs.readFile('test-input', 'utf-8', (err, data) => {
     let rawInstructions = data.split('\n');
     let instructions = createMovementInstructions(rawInstructions);
 
+    let headPosition = { x: 0, y: 0 };
+    let tailPosition = { x: 0, y: 0 };
+    let uniqueTailPositions = new Set();
+
+    for (const instruction of instructions) {
+        switch (instruction.direction) {
+            case 'U':
+                // inc y
+            case 'R':
+                // inc x
+            case 'D':
+                // dc y
+            case 'L':
+                // dc x
+        }
+
+        // check diagonal/head loc, move tail, add to Set
+    }
 
     console.log(instructions);
 });
